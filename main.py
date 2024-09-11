f50c417835836916b5479d48b7794011169919a2
fayajhussain <alifaiyaj102@gmail.com>
2024-09-11 11:24:29 +0545

    v1

---
+++ main.py
@@ -0,0 +1,29 @@
+import pyjokes
+jokes=pyjokes.get_joke()
+print(jokes)
+#print("nice joke")
+print('''Baa, baa, black sheep, have you any wool
+Yes, sir, yes, sir, three bags full
+One for the master, one for the dame
+One for the little boy who lives down the lane
+Baa, baa, black sheep, have you any wool?
+Yes, sir, yes, sir, three bags full
+Baa, baa, blue sheep, have you any wool?
+Yes, sir, yes, sir, three bags full
+One for the master, one for the dame
+One for the little boy who lives down the lane
+Baa, baa, blue sheep, have you any wool?
+Yes, sir, yes, sir, three bags full
+Baa, baa, pink sheep, have you any wool?
+Yes, sir, yes, sir, three bags full
+One for the master, one for the dame
+One for the little boy who lives down the lane
+Baa, baa, pink sheep, have you any wool?
+Yes, sir, yes, sir, three bags full
+Baa, baa, white sheep, have you any wool?
+Yes, sir, yes, sir, three bags full
+One for the master, one for the dame
+One for the little boy who lives down the lane
+Baa, baa, white sheep, have you any wool?
+Yes, sir, yes, sir, three bags full'''
+)
---
+++ strings.py
@@ -0,0 +1,10 @@
+name=("jaddu")
+reverse=name[::-1]
+print(reverse)
+print(len(name))
+print (name)
+nameshort=name[0:3]
+print (nameshort)
+skip=name[0:4:2]
+print (skip)
+
